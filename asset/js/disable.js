025da9a7f4a411cbb658bb28a7494fbde0a7bd9b
Jonathan Abiva <abivajonathan17@gmail.com>
2023-11-12 18:22:56 +0800

    new

--- asset/js/disable.js
+++ asset/js/disable.js
@@ -1,12 +1,12 @@
-document.addEventListener("keydown", function (event) {
-  event.preventDefault();
-});
+// document.addEventListener("keydown", function (event) {
+//   event.preventDefault();
+// });
 
-document.addEventListener("contextmenu", function (event) {
-  event.preventDefault();
-  showWarning();
-});
+// document.addEventListener("contextmenu", function (event) {
+//   event.preventDefault();
+//   showWarning();
+// });
 
-function showWarning() {
-  alert("right click are disabled on this page.");
-}
+// function showWarning() {
+//   alert("right click are disabled on this page.");
+// }
